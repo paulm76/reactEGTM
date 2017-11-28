e1f0d6be02172304012261f41aae3bfc38a2c367
Paul <paul.michon@devinci.fr>
2017-11-28 18:47:29 +0100

    ajout du fichier de difficulté

---
+++ src/constants/difficultyArray.js
@@ -0,0 +1,9 @@
+const difficultyArray = [
+	{ key: 'noDifficulty', value: 'noDifficulty', text: 'Indifférent' },
+	{ key: 'easy', value: 'easy', text: 'Facile' },
+	{ key: 'medium', value: 'medium', text: 'Moyen' },
+	{ key: 'dificult', value: 'difficult', text: 'Difficile' },
+	{ key: 'expert', value: 'expert', text: 'Expert' }
+];
+
+export default difficultyArray;
